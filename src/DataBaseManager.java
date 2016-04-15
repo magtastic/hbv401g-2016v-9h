d1543c345e4b32144cb1caa595e990bc9fd81905
magtastic <magnusol93@gmail.com>
2016-04-15 13:53:47 +0000

    stash

--- src/DataBaseManager.java
+++ src/DataBaseManager.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 public class DatabaseManager {
 	
 	public ArrayList<Hotel> getInitHotels(String location) {
